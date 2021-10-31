301f9067ef43424c1d47ec3b38637be5b93b5691
Andrew Rasmussen <andrew@a13n.com>
2021-10-30 20:20:43 -0400

    Log post data (for twitch verification)

--- src/server.js
+++ src/server.js
@@ -1,25 +1,43 @@
 const database = require('./database');
 const http = require('http');
 
-var i = database.get('i');
-
 class Server {
-  start() {
-    const server = http.createServer((request, response) => {
-      if (request.url !== '/') {
-        response.writeHead(404);
-        response.end();
-        return;
-      }
-
-      console.log('got request');
-      database.set('i', i + 1);
-      i = database.get('i');
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify({
-        data: i,
-      }));
+  async getPostData(request) { 
+    return new Promise((resolve, reject) => {
+      var body = '';
+      request.on('data', (data) => {
+        body += data;
+        if (body.length > 1000000) {
+          request.connection.destroy();
+          reject();
+        }
+      });
+      request.on('end', () => {
+        resolve(body);
+      });
     });
+  }
+
+  requestHandler = async (request, response) => {
+    if (request.method === 'POST') {
+      const data = await this.getPostData(request);
+      console.log('post data');
+      console.log(data);
+    }
+    if (request.url !== '/') {
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({
+      foo: 'bar',
+    }));
+  }
+
+  start() {
+    const server = http.createServer(this.requestHandler);
     server.listen(3000);
   }
 }
