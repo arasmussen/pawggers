fe06737a12c0a62aa3349724f414f0eafac25329
Andrew Rasmussen <andrew@a13n.com>
2021-11-10 01:37:17 +0000

    log reward-updated endpoint

--- src/endpoints/reward-updated.js
+++ src/endpoints/reward-updated.js
@@ -1,5 +1,7 @@
 module.exports = function(request, response) {
+  console.log('received reward-updated request');
   const data = request.postData;
+  console.log(data);
   response.writeHead(200, { 'Content-Type': 'application/json' });
   response.end(data.challenge);
 }
