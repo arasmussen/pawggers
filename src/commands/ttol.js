c3bd28f7ff6db66f415787228d05ef416b83ec37
Sarah Hum <sgyhum@gmail.com>
2022-01-23 18:01:00 -0800

    ttol bot commands

---
+++ src/commands/ttol.js
@@ -0,0 +1,41 @@
+const { ClientRequest } = require('http');
+const { client } = require('tmi.js');
+const database = require('../database');
+
+module.exports = function(context) {
+  const { client, target } = context;
+
+  // data validation
+  if (!context?.['display-name'] ||
+      !context?.['user-id']) {
+    console.log('validation failed');
+    return;
+  }
+
+  // get user
+  const user = {
+    id: context['user-id'],
+    name: context['display-name'],
+  };
+
+  // update database
+  let ttolTable = database.get('ttolTable');
+  ttolTable = ttolTable || {};
+  database.set('ttolTable', ttolTable);
+
+  // get guess
+  const guess = context.variables.join(' ').trim().toLowerCase();
+
+  // handle if guess isn't legit
+  if (guess !== 'a' && guess !== 'b' && guess !== 'c') {
+    client.say(target, `${user.name}, guess either A, B, or C.`);
+    return;
+  }
+
+  // add guess to list
+  ttolTable[user.id] = ttolTable[user.id] || {};
+  ttolTable[user.id].guess = guess || {};
+  database.set('ttolTable', ttolTable);
+
+  // consider whispering to user they guessed
+}
---
+++ src/commands/ttolstart.js
@@ -0,0 +1,35 @@
+const { ClientRequest } = require('http');
+const { client } = require('tmi.js');
+const database = require('../database');
+const isSarah = require('../util/isSarah');
+
+module.exports = function(context) {
+  const { client, target } = context;
+
+  // data validation
+  if (!context?.['display-name'] ||
+      !context?.['user-id']) {
+    console.log('validation failed');
+    return;
+  }
+
+  // get user
+  const user = {
+    id: context['user-id'],
+    name: context['display-name'],
+  };
+
+  // return if not sarah
+  if (!isSarah(user.id)) {
+    client.say(target, `Nice try, ${user.name}`);
+    return;
+  }
+
+  // update database
+  let ttolTable = database.get('ttolTable');
+  ttolTable = {};
+  database.set('ttolTable', ttolTable);
+
+  // print result
+  client.say(target, `Two Truths, One Lie game started, use !ttol [A/B/C] to guess which is the lie.`);
+}
