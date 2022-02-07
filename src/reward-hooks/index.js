979c99386bf16cece534f63442723c70c01f4606
Sarah Hum <sgyhum@gmail.com>
2022-02-06 20:59:25 -0800

    Pet emmy to index

--- src/reward-hooks/index.js
+++ src/reward-hooks/index.js
@@ -1,9 +1,11 @@
 const Rewards = {
   BuriedPawggers: '5aa81e72-5708-434c-ac2e-3dd45e94aa2f',
   First: '18e618a5-476f-4f63-a353-8c6ff9b5562c',
+  PetEmmy: 'fc268e39-e2f4-4676-a1b4-dfc8282d9336',
 };
 
 module.exports = {
   [Rewards.BuriedPawggers]: require('./dig'),
   [Rewards.First]: require('./first'),
+  [Rewards.PetEmmy]: require('./petemmy'),
 };
