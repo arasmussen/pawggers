afecde8d5a75d26004d1b5240fe9063752036429
Sarah Hum <sgyhum@gmail.com>
2022-05-04 00:07:24 -0400

    Emmyfest style changes

--- src/pages/emmyfest.js
+++ src/pages/emmyfest.js
@@ -13,25 +13,29 @@ const PageCSS = `
     font-family: 'Fredoka', sans-serif;
     text-align: center;
     box-sizing: border-box;
+    background: #1C1420;
   }
 
   h1 {
     margin: 0 0 50px 0;
     font-size: 50px;
     letter-spacing: 0.1em;
-    color: #8D46D7;
+    color: #CD62FF;
   }
 
   h2 {
     text-transform: uppercase;
-    font-size: 20px;
+    font-size: 18px;
     margin: 0 0 20px 0;
-    letter-spacing: 0.1em;
-    color: #8D46D7;
+    letter-spacing: 0.15em;
+    color: #E0D4EF;
+    font-weight: 500;
   }
 
   p {
     font-size: 18px;
+    color: #E0D4EF;
+    letter-spacing: 0.02em;
   }
 
   img {
@@ -45,11 +49,11 @@ const PageJS = ``;
 module.exports = function(request, response, server) {
 
   const body = `
-    <h1>EMMYFEST 2022</h1>
+    <h1>EMMYFEST <span>2022</span></h1>
     <h2>Milestones</h2>
     <img src="https://bit.ly/38RRj8g"</img>
     <h2>!tee</h2>
-    <img src="https://cdn.discordapp.com/attachments/891339850828087436/971251629892440104/tee-mock.png"</img>
+    <img src="https://cdn.discordapp.com/attachments/891339850828087436/971260143880785950/tee-mock.png"</img>
     <p>If y'all want, we can look into selling these after :)</p>
   `;
 
