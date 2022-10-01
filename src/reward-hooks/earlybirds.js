779c084faae6b50244386c602d951aa324e79a2a
Sarah Hum <sgyhum@gmail.com>
2022-10-01 11:36:46 -0400

    Changing first to early birds

--- src/reward-hooks/earlybirds.js
+++ src/reward-hooks/earlybirds.js
@@ -1,6 +1,7 @@
 const abbreviateNumber = require ('../util/abbreviateNumber');
 const { ClientRequest } = require('http');
 const database = require('../database');
+const getDay = require('../util/getDay');
 const getPeriod = require('../util/getPeriod');
 const twitch = require('../managers/twitch');
 
@@ -26,23 +27,58 @@ module.exports = function(data) {
   // get period
   const period = getPeriod();
 
+  // early birds database
+  const today = getDay();
+  let earlyBirdTable = database.get('earlyBirdTable');
+
+  earlyBirdTable = earlyBirdTable || {
+    earlyBirds: [],
+  };
+
+  // add user to list
+  const newEarlyBird = {
+    date: today,
+    username: user.name,
+  };
+  earlyBirdTable.earlyBirds.push(newEarlyBird);
+  database.set('earlyBirdTable', earlyBirdTable);
+
+  // calculate how many pawggers
+  var pawggers = 0;
+  var place = '';
+  if (earlyBirdTable.earlyBirds.length === 1) {
+    pawggers = 1000;
+    place = 'FIRST';
+  } else if (earlyBirdTable.earlyBirds.length === 2) {
+    pawggers = 500;
+    place = 'SECOND';
+  } else {
+    pawggers = 250;
+    place = 'THIRD';
+    earlyBirdTable.earlyBirds = [];
+  }
+  database.set('earlyBirdTable', earlyBirdTable);
+
   // update database
   let userSpendTable = database.get('userSpendTable');
   userSpendTable = userSpendTable || {};
   userSpendTable[period] = userSpendTable[period] || {};
   userSpendTable[period][user.id] = userSpendTable[period][user.id] || {};
   userSpendTable[period][user.id].spend = userSpendTable[period][user.id].spend || 0;
-  userSpendTable[period][user.id].spend += 1000;
+  userSpendTable[period][user.id].spend += pawggers;
   database.set('userSpendTable', userSpendTable);
 
   // get total pawggers
   const spend = abbreviateNumber(Number(userSpendTable[period][user.id].spend));
-
-  const storyList = ['The battle was fierce but', 'That was a tough battle but', 'That was an intense fight but', 'We got some lurkers but who will win?', 'Early bird catches the worm.'];
-  const resultList = ['snatches first', 'comes out on top', 'grabs first, and the crowd goes wild', 'winnnnnnns'];
-  const story = storyList[Math.floor(Math.random() * storyList.length)];
-  const result = resultList[Math.floor(Math.random() * resultList.length)];
+  
+  // print out numbers
+  var prettyPawggers = '';
+  if (pawggers === 1000) {
+    prettyPawggers = '1,000';
+  } else {
+    prettyPawggers = pawggers.toString();
+  }
 
   // print result
-  twitch.client.say('#xhumming', `${story} ${user.name} ${result}! They now have ${spend} pawggers.`);
+  twitch.client.say('#xhumming', `${user.name} comes in ${place} to claim a worm for ${prettyPawggers} pawggers! They now have ${spend} pawggers.`);
 }
