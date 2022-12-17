1df911f7c960202433495312cee79ed64d5614b6
Sarah Hum <sgyhum@gmail.com>
2022-12-17 10:44:14 -0500

    Adding past charities

--- src/pages/wall-of-fame.js
+++ src/pages/wall-of-fame.js
@@ -32,22 +32,33 @@ const PageCSS = `
     font-weight: 600;
   }
 
+  .stream {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 50%;
+    margin: 0 0 40px 0;
+  }
+
   p {
-    font-size: 18px;
+    font-size: 16px;
     color: #333;
     max-width: 550px;
+  }
+
+  .dot {
+    color: #d19588;
   }
 
   .date {
     margin: 0 0 20px 0;
     font-size: 14px;
-    opacity: 0.5;
+    color: #d19588;
     font-weight: 400;
   }
 
   img {
     width: 700px;
-    max-width: 50%;
     border-radius: 10px;
     margin: 0 0 30px 0;
   }
@@ -56,13 +67,31 @@ const PageJS = ``;
 
 module.exports = function(request, response, server) {
 
+  const streamForAllAnimals = ['urbanbohemian','deekim','tiltify','Jayshoon','TheBigJuicy89','HemiDex','sunfflawer','D21winters','lilsmurfyone','pranayyy','sassykatasstrophe','pixelatedsoul','toastwithjam_','MisstyMoo','jerklynn','heytherehan','dianeland','cerealdotcom','NovaMoocakes','w0ngaccount','Forelorne','PlayItRay','theEvilChow','staymadge','saltedsaltine','kittskats','mightymur','lunar_mariya','Chebyyyyyy','ayo_frankie','doaflipguy'];
+  const emmyfest2022 = ['j3dg','pranayyy','sunfflawer','morgan_victoria','hedgehog166','QA_ninja','staymadge','AlviarinRhul','vlc999','lunar_marya','pmgomz','chrischin1','Its_Cheby','w0ngaccount','hemidex84','Partiick0331','RumpleStudy','mae_prol','thecrimsondd','timmychaan','greplogic','xangrykidx','ryantupo','YUMHUMS','XEPHAN0','johnkeiwo','ChaosForTheFly','Forelorne','jayshoon','rngie','Lilsmurfyone','saltedsaltine','miklmusic','jjlintag','Vubar_GMAT','razzyrl','davigator2','JurassicPugg','jwang956','antibuenosocialclub','ckL18','7kats','PlayItRay','inSAMniak','reanimagus','YakkyStudy','xkatie93'];
+  const ukraine = ['Canny','inSAMniak','Kayla','Chris and Jordan','StayMadge','notCaitlyn','notImad','notRngie','greplogic','razzy','burbee','TheBigJuicy89','d4wnwolf','NotDruhvay','Razzy','Nori','w0ngaccount','Jhecht','RocketNine','HemiDex','breadgirl','philippe_pj','ChaosForTheFly','LunarMarya','Bauhaus_Banshee','NotJayshoon','Kevin','kittskats','Paolo','7kats','SoulDude','Alex','davigator2','Susanne','duck_027','AlviarinRhul','D21winters','Joe','yumhums','samuelspeak','Rob'];
+
   const body = `
     <h1>EmmyHQ Wall of Fame</h1>
     <p>Documenting the champions from EmmyHQ who have gone above and beyond to give to those in&nbsp;need.</p>
-    <h2>Stream for All Animals</h2>
-    <p class="date">December 15-18, 2022</p>
-    <img src="https://media.discordapp.net/attachments/891339850828087436/1053544835560247306/image.png?width=1138&height=639"</img>
-    <p>urbanbohemian · deekim · tiltify · jayshoon · TheBigJuicy89 · HemiDex · sunfflawer · D21winters · lilsmurfyone · pranayyy · sassykatasstrophe · pixelatedsoul · toastwithjam_ · MisstyMoo · jerklynn · heytherehan · dianeland · cerealdotcom · NovaMoocakes · w0ngaccount · Forelorne · PlayItRay · theEvilChow · staymadge · saltedsaltine · kittskats · mightymur · lunar_mariya · Chebyyyyyy · ayo_frankie · doaflipguy · cbhl</p>
+    <div class="stream">
+      <h2>Stream for All Animals</h2>
+      <p class="date">December 15-18, 2022</p>
+      <img src="https://media.discordapp.net/attachments/891339850828087436/1053544835560247306/image.png?width=1138&height=639"</img>
+      <p>${streamForAllAnimals.join("<span class='dot'> · </span>")}</p>
+    </div>
+    <div class="stream">
+      <h2>EMMYFEST 2022 for F Cancer</h2>
+      <p class="date">May 14, 2022</p>
+      <img src="https://cdn.discordapp.com/attachments/891339850828087436/1053695590409715752/image.png"</img>
+      <p>${emmyfest2022.join("<span class='dot'> · </span>")}</p>
+    </div>
+    <div class="stream">
+      <h2>Project Hope for Ukraine</h2>
+      <p class="date">March 8-13, 2022</p>
+      <img src="https://media.discordapp.net/attachments/891339850828087436/1053693775282708510/image.png?width=960&height=539"</img>
+      <p>${ukraine.join("<span class='dot'> · </span>")}</p>
+    </div>
   `;
 
   // respond
