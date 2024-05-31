d00de264a4dac08b7ff1433e765b8bcad5cfc02f
Sarah Hum <sgyhum@gmail.com>
2024-05-31 11:03:39 -0700

    Updating mod list

--- src/util/isMod.js
+++ src/util/isMod.js
@@ -3,9 +3,11 @@ const ModIDs = [
   '594470471', // cait
   '738584885', // emmydotjs
   '571790466', // flawer
+  '614073889', // smith
   '151640996', // imad
   '608113661', // mariya
   '509514282', // rngie
+  '113816041', // saltine
   '679555136', // xhumming
   '470611865', // razzy
 ];
